09821a551fc69b794966a014b12e948f8f25876c
Denis Yakimov <daria.pindus@gmail.com>
2016-04-14 21:16:13 +0300

    changed strnigs

--- ua/od/pindus/StringOperations.java
+++ ua/od/pindus/StringOperations.java
@@ -9,5 +9,31 @@ public class StringOperations {
         String str="Hello world";
         char ch=str.charAt(3);
         System.out.println("Character at position 3 : "+ch);
+
+        /*str=str+"!!!";
+        System.out.println(str);
+
+        str=str.concat("???");
+        System.out.println(str);
+
+        String str2="\"Test\"";
+
+        String str3="       Test spaces      ";
+        System.out.println(str3);
+        str3=str3.trim();
+        System.out.println(str3);
+
+        str3=str3.replace("T", "t");
+        System.out.println(str3);*/
+
+        String str4="string";
+        str4=str4.substring(2,4);
+        System.out.println(str4);
+
+        String hello="hello sick  sad world";
+        String[] arr=hello.split(" ");
+
+        for(int i=0; i<arr.length; i++)
+            System.out.println(arr[i]);
     }
 }
